eb0232c8688d32a9a59a7ad25df6d8ffad4eedcb
Manfred Steyer <manfred.steyer@gmx.net>
2022-10-08 15:13:24 +0200

    update to nf 1.0.0

---
+++ update.bat
@@ -0,0 +1,3 @@
+call npm i @softarc/native-federation@latest --registry http://localhost:4873
+call npm i @softarc/native-federation-runtime@latest --registry http://localhost:4873
+call npm i @softarc/native-federation-esbuild@latest --registry http://localhost:4873
